b2edbea129159049740e135a8981c02f1ab3c19c
Rok Andrée <rok@andree.si>
2021-04-10 12:04:18 +0200

    Removes file that should be in gitignore

--- storage/framework/models.scanned.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$router->model('adrema', 'App\Models\Adrema');
-$router->model('sentMessage', 'App\Models\SentMessage');
-$router->model('verification', 'App\Models\Verification');
-$router->model('voter', 'App\Models\Voter');
