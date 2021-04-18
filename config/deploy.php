d57c09d75b17862b873e0a536e7c4b0a9bb4410f
Demjan <demjan.vester@gmail.com>
2021-04-18 21:46:05 +0200

    deploy env

--- config/deploy.php
+++ config/deploy.php
@@ -113,8 +113,8 @@ return [
 
     'hosts' => [
         'staging' => [
-            "hostname" => "vps2",
-            'deploy_path' => '/var/www/sender.stage.eglasovanje.si',
+            "hostname" => env('DEPLOY_HOSTNAME_STAGING', ''),
+            "deploy_path" => env('DEPLOY_DIRECTORY_STAGING', ''),
             "stage" => "staging",
             'http_user' => "http",
             'shared_files' => ['.env', 'etc/nginx.conf'],
@@ -122,8 +122,8 @@ return [
             'user' => 'deploy',
         ],
         'production' => [
-            "hostname" => "vps2",
-            'deploy_path' => '/var/www/sender.eglasovanje.si',
+            "hostname" => env('DEPLOY_HOSTNAME_PRODUCTION', ''),
+            "deploy_path" => env('DEPLOY_DIRECTORY_PRODUCTION', ''),
             "stage" => "production",
             'http_user' => "http",
             'shared_files' => ['.env', 'etc/nginx.conf'],
