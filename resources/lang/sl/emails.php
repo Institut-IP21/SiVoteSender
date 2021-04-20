ee5b383629458e35bd4d2f82365b16a18a21102b
Demjan <demjan.vester@gmail.com>
2021-04-20 23:46:10 +0200

    many translations

---
+++ resources/lang/sl/emails.php
@@ -0,0 +1,13 @@
+<?php
+
+
+return [
+    'verification' => [
+        'subject' => 'E-voting: please verify your contact information!',
+        'btnConfirm' => 'Potrdite email naslov'
+    ],
+    'invite' => [
+        'subject' => 'E-voting: your ballot',
+        'btnConfirm' => 'Odprite glasovnica'
+    ]
+];
---
+++ resources/lang/sl/verification.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'success' => [
+        'title' => 'Email naslov uspešno verificiran',
+        'text' => 'Hvala za sodelovanje'
+    ]
+];
