97ea3146ec3164b9bb9e1d1b4a5d682460fb98db
Demjan <demjan.vester@gmail.com>
2021-04-22 22:32:43 +0200

    Adjust width

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,9 +2,9 @@
     <td class="header">
         <a href="https://eglasovanje.si" style="display: inline-block;">
             @if (!empty($personalization) && $personalization->photo_url)
-            <img src="{{$personalization->photo_url}}" alt="Logo" width="100%" style="height: 75px">
+                <img src="{{ $personalization->photo_url }}" alt="Logo" width="auto" style="height: 75px">
             @else
-            eGlasovanje
+                eGlasovanje
             @endif
         </a>
     </td>
