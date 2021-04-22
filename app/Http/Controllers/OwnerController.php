cf8d64e8559779f11405cc79cc6154f871e877b1
Demjan <demjan.vester@gmail.com>
2021-04-22 22:32:50 +0200

    unused imports

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -2,18 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\AdremaBasic;
-use App\Http\Resources\AdremaFull;
 use App\Http\Resources\PersonalizationFull;
-use App\Http\Resources\VoterBasic;
-use App\Models\Adrema;
 use App\Models\Personalization;
-use App\Models\Voter;
-use App\Services\Ballot;
-use Illuminate\Database\Eloquent\Builder;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 /**
  * @Controller(prefix="api/owner")
