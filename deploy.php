034b69ad8e857324125ad2093b90e47e12255aa4
Demjan <demjan.vester@gmail.com>
2021-08-17 21:18:39 +0200

    deploy file

---
+++ deploy.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Deployer;
+
+$env = \Dotenv\Dotenv::createImmutable(__DIR__)->load();
+
+require 'recipe/laravel.php';
+require 'recipe/yarn.php';
+require 'contrib/php-fpm.php';
+require 'contrib/npm.php';
+
+set('application', 'SiVoteEngine');
+set('repository', 'git@github.com:Institut-IP21/SiVoteSender');
+set('php_fpm_version', '7.4');
+
+host('staging')
+    ->set('labels', ['stage' => 'staging'])
+    ->set('hostname', function () {
+        return env('DEPLOY_HOSTNAME_STAGING');
+    })
+    ->set('remote_user', function () {
+        return env('DEPLOY_USER_STAGING');
+    })
+    ->set('deploy_path', function () {
+        return env('DEPLOY_DIRECTORY_STAGING');
+    })
+    ->set('shared_files', ['.env', 'etc/nginx.conf'])
+    ->set('shared_dirs', ['storage']);
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:optimize',
+    'artisan:model:scan',
+    'artisan:route:scan',
+    'artisan:migrate',
+    'deploy:publish',
+]);
+
+task('yarn', function () {
+    cd('{{release_or_current_path}}');
+    run('yarn');
+});
+
+task('yarn:production', function () {
+    cd('{{release_or_current_path}}');
+    run('yarn production');
+});
+
+task('artisan:model:scan', function () {
+    cd('{{release_or_current_path}}');
+    echo run('php artisan model:scan');
+});
+
+task('artisan:route:scan', function () {
+    cd('{{release_or_current_path}}');
+    echo run('php artisan route:scan');
+});
+
+after('deploy:failed', 'deploy:unlock');
