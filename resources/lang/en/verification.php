d648901008418fabeba9a2672c36abada726ce85
VesterDe <demjan.vester@gmail.com>
2026-06-05 02:17:12 +0200

    Add missing translations and fill Slovene
    Create en.json/sl.json, add verification.success.button, fill Slovene counterparts.

--- resources/lang/en/verification.php
+++ resources/lang/en/verification.php
@@ -5,6 +5,7 @@ return [
     'success' => [
         'title' => 'Verification successful',
         'text' => 'Your ability to receive emails that contain links to ballots has been verified.',
+        'button' => 'You can close this page.',
     ],
     'email_subject_template' => 'eglasovanje.si - Potrdite svojo zmožnost glasovanja',
     'email_body_template' => 'S klikom na spodnjo povezavo boste potrdili svojo zmožnost glasovanja preko sistema eglasovanje.si'
--- resources/lang/sl/verification.php
+++ resources/lang/sl/verification.php
@@ -6,6 +6,9 @@ return [
         'title' => 'Potrditev kontakta uspešna',
         'text' => 'Vaša zmožnost, da prejemate e-mail sporočila, ki jih pošilja <a href="https://eglasovanje.si">eglasovanje.si</a> in bodo vsebovala povezavo do glasovnice je bila uspešno potrjena.',
         'button' => 'To stran lahko zaprete.'
-    ]
+    ],
+
+    'email_subject_template' => 'eglasovanje.si - Potrdite svojo zmožnost glasovanja',
+    'email_body_template' => 'S klikom na spodnjo povezavo boste potrdili svojo zmožnost glasovanja preko sistema eglasovanje.si',
 
 ];
